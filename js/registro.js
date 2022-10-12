c0a7f6d3df557f2183ef0b1c552cfa9c87cc3af3
LuisJavier Hernandez <luisjavierhernandez150@gmail.com>
2022-10-12 18:23:32 -0500

    Actualizado

---
+++ js/registro.js
@@ -0,0 +1,25 @@
+//Declaracion de variables de referencia a objetos
+const nombre= document.getElementById('name');
+const email = document.getElementById('email');
+const user = document.getAnimations('user');
+const pass = document.getAnimations('pass');
+const formulario = document.getElementById('formRegistro')
+
+//
+formulario.addEventListener('submit', registro)
+
+// 
+function registro(e){
+    e.preventDefault();
+    
+    let nombreVal = nombre.value;
+    let emailVal = email.value;
+    let userVal = user.value;
+    let passVal = pass.value;
+
+    if(nombreVal == '' || emailVal == '' || userVal == '' || passVal == ''){
+        return;
+    }
+    console.log('Datos validados');
+
+}
