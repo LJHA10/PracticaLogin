4ed749f2003abf0335ff322989ccc8bc87c18fb1
LuisJavier Hernandez <luisjavierhernandez150@gmail.com>
2022-10-14 07:28:00 -0500

    Ya esta padrino

--- js/index.js
+++ js/index.js
@@ -1,22 +1,37 @@
-//Declaracion de variables para vincular los objetos del formulario
-const usuario= document.getElementById('user');
-const password= document.getElementById('pass');
-const formulario=document.getElementById('forLogin');
+// decalrara variables para vincular los objetso de formulario
+const usuario = document.getElementById('user');
+const password = document.getElementById('pass');
+const formulario = document.getElementById('formLogin');
 
-//Generando eventos
+//Genrando eventos
 formulario.addEventListener('submit', login);
 
-//Funciones
+//Funciones a ejecutar
 function login(e){
-    e.preventDefault();
-    
-    let usuarioVal = usuario.value;
-    let passwordVal = password.value;
+e.preventDefault();
 
-    if(usuarioVal == '' || passwordVal == ''){
+let usuarioVal = usuario.value;
+let passwordVal = password.value;
+
+if(usuarioVal == '' || passwordVal == ''){
+    creaMensaje('Verifica tus campos','danger');
     return;
+}
+
+if(localStorage.getItem('usuario')){
+    let objeto = JSON.parse(localStorage.getItem('usuario'));
+
+    if( usuarioVal == objeto.user && passwordVal == objeto.pass){
+        creaMensaje('Login correcto','success');
+        localStorage.setItem('sesion','activa');
+        setTimeout(function(){
+            window.open('./inicio.html','_self');
+        },2000);
+    }else{
+        creaMensaje('Uusaurio Incorrecto','danger');
     }
-    console.log('EL valor para user es: '+ usuarioVal);
-    console.log('EL valor para password es: '+ passwordVal);
-    
+}else{
+    creaMensaje('No hay registros','danger');
+}
+
 }
--- js/registro.js
+++ js/registro.js
@@ -1,46 +1,36 @@
 //Declaracion de variables de referencia a objetos
-const nombre= document.getElementById('name');
+const nombre = document.getElementById('name');
 const email = document.getElementById('email');
-const user = document.getAnimations('user');
-const pass = document.getAnimations('pass');
-const formulario = document.getElementById('formRegistro')
+const user = document.getElementById('user');
+const pass = document.getElementById('pass');
+const formulario = document.getElementById('formRegistro');
 
-//
 formulario.addEventListener('submit', registro);
 
-// 
 function registro(e){
     e.preventDefault();
-    
+
     let nombreVal = nombre.value;
     let emailVal = email.value;
     let userVal = user.value;
-    let passVal = pass.value;
+    let passValue = pass.value;
 
-    if(nombreVal == '' || emailVal == '' || userVal == '' || passVal == ''){
-        creaMensaje('Verifica tus campos', 'danger');
-        return;
+    if(nombreVal =='' || emailVal == '' || userVal == '' || passValue == ''){
+         creaMensaje('Verifa tus campos','danger');
+         return;
     }
     const usuario = {
         nombre: nombreVal,
         email: emailVal,
         user: userVal,
-        pass: passVal
+        pass: passValue,
     }
     localStorage.setItem('usuario', JSON.stringify(usuario));
 
-        nombre.value= '';
-        email.value= '';
-        user.value='';
-        pass.value='';
+    nombre.value = '';
+    email.value = '';
+    user.value = '';
+    pass.value = '';
 
-    creaMensaje('Usuario Registrado', 'success');
-}
-
-function creaMensaje(texto, tipo){
-    const nuevoElemeto=document.createElement('div');
-    nuevoElemeto.innerText = texto;
-    nuevoElemeto.classList.add('alert', 'alert-' + tipo);
-    const divMensaje = document.getElementById('mensaje');
-    divMensaje.appendChild(nuevoElemeto);
+    creaMensaje('Usuario registrado!','success');
 }
